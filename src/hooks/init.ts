372b3e472b8bfab85f3bf6b7ffe238115973e307
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-14 20:53:07 -0700

    wip

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -4,14 +4,13 @@ import * as path from 'path'
 import {Subject} from '..'
 
 const hook: Hook<'init'> = async function (opts) {
-  const id = opts.id
+  const {id, argv} = opts
   if (id && id.startsWith('@')) {
     try {
       const subjectRoot = path.join(this.config.root, 'src/subjects')
-      const subjectName = id.split('@')[1]
+      const subjectName = id.split('@')[1].split(':')[0]
       const subject: Subject<any> = require(path.join(subjectRoot, subjectName)).default
-      const verb = subject.verbs.find(c => c.verb === (opts.argv[0] || 'list'))
-      await verb!.run()
+      await subject.run(id, argv)
       this.exit(0)
     } catch (err) {
       this.error(err)
--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,16 @@ export interface Subject<T extends object> {
   verbs: Array<typeof Verb>
   fetchAll(): Promise<T[]>
   fetchOne(id: string): Promise<T>
+  run(id: string, argv: string[]): Promise<any>
 }
 
 export function subject<T extends object>(options: SubjectOptions<T>): Subject<T> {
   return {
     ...options,
+    async run(_: string, argv: string[]) {
+      const verb = this.verbs.find(c => c.verb === (argv[0] || 'list'))
+      await verb!.run()
+    },
     verbs: options.verbs.map(v => {
       if (typeof v !== 'string') return v
       switch (v) {
